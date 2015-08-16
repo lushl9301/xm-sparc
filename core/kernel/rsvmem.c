47c25eb2bdd143135fa84d27cbd31da782dda6c0
lushl9301 <lushl9301@gmail.com>
2015-08-16 21:49:05 +0800

    rsvmem.c

--- core/kernel/rsvmem.c
+++ core/kernel/rsvmem.c
@@ -19,16 +19,17 @@
 void InitRsvMem(void) {
     xm_s32_t e;
     for (e=0; xmcRsvMemTab[e].obj; e++)
-	xmcRsvMemTab[e].usedAlign&=~RSV_MEM_USED;
+        xmcRsvMemTab[e].usedAlign&=~RSV_MEM_USED;
 }
 
 void *AllocRsvMem(xm_u32_t size, xm_u32_t align) {
     xm_s32_t e;
-    for (e=0; xmcRsvMemTab[e].obj; e++) {        
-	if (!(xmcRsvMemTab[e].usedAlign&RSV_MEM_USED)&&((xmcRsvMemTab[e].usedAlign&~RSV_MEM_USED)==align)&&(xmcRsvMemTab[e].size==size)) {
+    ///??? only use size to justify?
+    for (e=0; xmcRsvMemTab[e].obj; e++) {
+        if (!(xmcRsvMemTab[e].usedAlign&RSV_MEM_USED)&&((xmcRsvMemTab[e].usedAlign&~RSV_MEM_USED)==align)&&(xmcRsvMemTab[e].size==size)) {
             xmcRsvMemTab[e].usedAlign|=RSV_MEM_USED;
-	    return (void *)((xmAddress_t)xmcRsvMemTab[e].obj+(xmAddress_t)&xmcTab);
-	}
+            return (void *)((xmAddress_t)xmcRsvMemTab[e].obj+(xmAddress_t)&xmcTab);
+	    }
     }
     return 0;
 }
