1962c2a212d88d395bb3d24c9ddbf4df4624df5e
lushl9301 <lushl9301@gmail.com>
2015-09-23 14:52:22 +0800

    bugs from Tom Sun

--- core/kernel/sched.c
+++ core/kernel/sched.c
@@ -328,7 +328,7 @@ void Schedule(void) {
 			kprintf("newK: idle ");
 
 		if (sched->cKThread->ctrl.g)
-			kprintf("curK: [%d:%d] 0x%x\n", KID2PARTID(sched->cKThread->ctrl.g->id), KID2VCPUID(sched->cKThread->ctrl.g->id));
+			kprintf("curK: [%d:%d] 0x%x\n", KID2PARTID(sched->cKThread->ctrl.g->id), KID2VCPUID(sched->cKThread->ctrl.g->id), sched->cKTHread);
 		else
 			kprintf("curK: idle\n");
 #endif
