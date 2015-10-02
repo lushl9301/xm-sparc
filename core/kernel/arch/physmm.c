6ecd19ebcc7863a2effee5598e5ab6942a0a8850
lushl9301 <lushl9301@gmail.com>
2015-10-02 11:25:21 +0800

    arch physmm.c

--- core/kernel/arch/physmm.c
+++ core/kernel/arch/physmm.c
@@ -1,7 +1,7 @@
 /*
  * $FILE: physmm.c
  *
- * Physical memory manager 
+ * Physical memory manager
  *
  * $VERSION$
  *
@@ -16,11 +16,10 @@
 #include <arch/physmm.h>
 
 //TODO
-
 xmAddress_t EnableByPassMmu(xmAddress_t addr, partition_t * p, struct physPage ** page){
     return addr;
 }
 
+//TODO not implemented
 inline void DisableByPassMmu(struct physPage * page){
 }
-
