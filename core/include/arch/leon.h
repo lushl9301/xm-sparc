4f686fb5147a22040141396ca0deebf5eeab8652
lushl9301 <lushl9301@gmail.com>
2015-10-05 14:39:14 +0800

    leon.h finished

--- core/include/arch/leon.h
+++ core/include/arch/leon.h
@@ -70,7 +70,7 @@
 // can write together with the reset
 
 static inline void StoreIoReg(xmAddress_t pAddr, xm_u32_t value) {
-// used a lot by hardware-denpendented code; simple line of asm
+//invoked by hardware-denpendented code a lot; simple line of asm
 #if defined(CONFIG_MMU)
     __asm__ __volatile__("sta %0, [%1] %2\n\t"::"r"(value),
                         "r"(pAddr), "i"(LEON_MMU_BYPASS):"memory");
